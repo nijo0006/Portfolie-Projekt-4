aa27d5b9c4b023ce7cbbb3e68b8a773171be9450
nijo0006 <nijo0006@stud.ek.dk>
2025-11-06 12:21:27 +0100

    -

--- js.js
+++ js.js
@@ -1,2 +1,20 @@
 const ctx = document.querySelector('#chart').getContext('2d');
 const chart = new Chart(ctx, {})
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "DBUSER",
+    password:"DBPASSWORD",
+    database: "Chinook"
+});
+//This is boilerplate code to write a simple web-server
+const http = require("http");
+
+http.createServer((req,res)=>{
+    res.write("Hello Everyone");
+    res.end();
+}).listen(8000);
+
+app.get("/hello", ()=> {
+    Console.log("Hey alle sammen");
+});
