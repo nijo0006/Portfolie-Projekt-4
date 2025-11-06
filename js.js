d8ddaa67e9255d160af654258ad1034e3d566292
naghdeh <jnaghdeh@gmail.com>
2025-11-06 13:01:27 +0100

    Lavet en query der viser mest solgte genre per land i alfabetisk rækkefølge

--- js.js
+++ js.js
@@ -31,3 +31,17 @@ app.get("/hello", (req, res) => {
     console.log("Hey alle sammen");
     res.send("Hello Everyone");
 });
+
+
+app.get("/basicquery",(req,res)=>{
+    connection.query(`SELECT i.BillingCountry, g.Name AS Genre, SUM(il.Quantity) AS QuantitySold
+    FROM InvoiceLine il
+    JOIN Invoice i ON il.InvoiceId = i.InvoiceId
+    JOIN Track t ON il.TrackId = t.TrackId
+    JOIN Genre g ON t.GenreId = g.GenreId
+    GROUP BY i.BillingCountry, g.Name
+    ORDER BY i.BillingCountry, QuantitySold DESC;`,
+    (err,results)=>{
+        res.send(results);
+    });
+});
