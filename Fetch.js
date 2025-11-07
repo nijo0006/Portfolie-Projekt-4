289fbbb70be019652c5e345515029f69d34a2bae
Loui <Loui.fatum@gmail.com>
2025-11-07 12:33:22 +0100

    fetch  sidste URL

--- Fetch.js
+++ Fetch.js
@@ -10,7 +10,7 @@ const revenue=fetch('https://raw.githubusercontent.com/nijo0006/Portfolie-Projek
         console.log(data);
     });
 
-const MostSoldGenre=fetch('skriv den sidste URL')
+const MostSoldGenre=fetch('https://raw.githubusercontent.com/nijo0006/Portfolie-Projekt-4/refs/heads/main/data-genre.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
