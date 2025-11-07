c0b8d8a987ab3ed54e16ffe9849a61a1ac8de505
Loui <Loui.fatum@gmail.com>
2025-11-07 12:23:24 +0100

    fetch mangler dne sidste URL

--- Fetch.js
+++ Fetch.js
@@ -1,4 +1,16 @@
-fetch('https://raw.githubusercontent.com/nijo0006/Portfolie-Projekt-4/refs/heads/main/data-sold.json')
+const sold=fetch('https://raw.githubusercontent.com/nijo0006/Portfolie-Projekt-4/refs/heads/main/data-sold.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
+
+const revenue=fetch('https://raw.githubusercontent.com/nijo0006/Portfolie-Projekt-4/refs/heads/main/data-revenue.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
+
+const MostSoldGenre=fetch('skriv den sidste URL')
     .then(response => response.json())
     .then(data => {
         console.log(data);
